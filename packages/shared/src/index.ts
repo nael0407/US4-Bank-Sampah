72119f9f11abbda555f94ffb4da2ba20248f0d86
Natanael Simanjuntak <natansimanjuntak17@gmail.com>
2026-09-26 17:51:30 +0700

    feat(shared): add user schemas

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -4,3 +4,4 @@ export * from "./schemas/common.schema";
 export * from "./schemas/saldo.schema";
 export * from "./schemas/setoran.schema";
 export * from "./schemas/titik-jemput.schema";
+export * from "./schemas/user.schema";
